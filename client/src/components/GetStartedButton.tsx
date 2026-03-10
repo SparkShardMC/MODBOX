6bcba37b5c575ac43e0b5464bcacae5f8638bb41
SparkShardMC <zaki.coding0119@gmail.com>
2026-03-10 14:45:56 -0700

    Create GetStartedButton.tsx

---
+++ client/src/components/GetStartedButton.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "../../styles/landing.css"
+
+export default function GetStartedButton() {
+    return (
+        <button className="get-started-button">GET STARTED</button>
+    )
+}

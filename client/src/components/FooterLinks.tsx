75f8663697a96cf98b73efa4e59290ed503fe337
SparkShardMC <zaki.coding0119@gmail.com>
2026-03-10 14:46:25 -0700

    Create FooterLinks.tsx

---
+++ client/src/components/FooterLinks.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "../../styles/landing.css"
+
+export default function FooterLinks() {
+    return (
+        <div className="footer-links">
+            <a href="#">Terms</a> | <a href="#">Privacy</a>
+        </div>
+    )
+}
